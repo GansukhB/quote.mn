a37ac96f10da9f5cdf95e5436d0e13fda11fe655
GansukhB <bganaa2009@gmail.com>
2014-03-31 23:16:52 -0700

    git ignore

--- lib/vendor/symfony/lib/plugins/sfDoctrinePlugin/data/generator/sfDoctrineModule/default/template/templates/editSuccess.php
+++
@@ -1,3 +0,0 @@
-<h1><?php echo sfInflector::humanize($this->getSingularName()) ?> <small>Засварлах</small></h1>
-<hr />
-[?php include_partial('form', array('form' => $form)) ?]
--- lib/vendor/symfony/lib/plugins/sfDoctrinePlugin/data/generator/sfDoctrineModule/default/template/templates/newSuccess.php
+++
@@ -1,3 +0,0 @@
-<h1><?php echo sfInflector::humanize($this->getSingularName()) ?> <small>Шинээр оруулах</small></h1>
-<hr />
-[?php include_partial('form', array('form' => $form)) ?]
